54ef53039e34449eb50b5c4eb68a1e5027889d24
sanyaswb <sanyaswb@gmail.com>
2022-11-19 09:41:47 +0200

    completed 29_sum-of-string-hard

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 function sumOfString(str) {
-	// write your code here
+	const arrStr = str.split('');
+	let sum = 0;
+	let selector = false;
+
+	for (let i = 0; i < arrStr.length; i++) {
+
+		if (!isNaN(+arrStr[i]) && selector === false) {
+			sum += +arrStr[i];
+		} else if (arrStr[i] === '-') {
+			selector = true;
+			continue;
+		} else if (!isNaN(+arrStr[i])) {
+			sum += (+arrStr[i] * -1);
+			selector = false;
+			continue;
+		} else {
+			selector = false;
+			continue;
+		}
+	}
+
+	return sum;
 }
 
 module.exports = sumOfString;
